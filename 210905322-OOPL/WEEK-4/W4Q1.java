42247283046701045f1515405374654b28048ad9
RonSaini <112055355+RonSaini@users.noreply.github.com>
2022-09-12 16:20:35 +0530

    Add files via upload

---
+++ 210905322-OOPL/WEEK-4/W4Q1.java
@@ -0,0 +1,58 @@
+import java.util.*;
+class Complex 
+{
+    double real;
+    double imag;
+    
+    public Complex()
+    {
+        real = 0;
+        imag = 0;
+    }
+    
+    public Complex(double real, double imag) 
+    {
+        this.real = real;
+        this.imag = imag;
+    }
+    
+    public Complex add(Complex n1, Complex n2)
+    {
+        Complex temp = new Complex(0.0, 0.0);
+
+        temp.real = n1.real + n2.real;
+        temp.imag = n1.imag + n2.imag;
+        display(temp);
+        return(temp);
+    }
+    
+    public Complex subtract(Complex n1, Complex n2)
+    {
+        Complex temp = new Complex(0.0, 0.0);
+
+        temp.real = Math.abs(n1.real - n2.real);
+        temp.imag = Math.abs(n1.imag - n2.imag);
+        display(temp);
+        return(temp);
+    }
+
+    public void display(Complex temp)
+    {
+        System.out.println("\nResult = " +temp.real+ " + " + "i" + temp.imag);
+    }
+
+}
+
+
+class W4Q1
+{
+    public static void main(String[] args) 
+    {
+        Complex n1 = new Complex(2.3, 4.5);
+        Complex n2 = new Complex(3.4, 5.0);
+        Complex temp = new Complex(0,0);
+
+        temp.add(n1, n2);
+        temp.subtract(n1,n2);
+    }
+}
